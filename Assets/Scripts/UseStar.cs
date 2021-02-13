f41a8d7ccc9c4edc9db384da5161474124b81fa1
Nicholas10128 <46949284+Nicholas10128@users.noreply.github.com>
2021-02-13 19:26:23 +0800

    Fixed use star count input field.
    The input field of uGUI cannot input the negative flag when using Integer type on Android. So change the input type from Integer to DecimalNumber. And add some codes to remove the comma and dot flags.

--- Assets/Scripts/UseStar.cs
+++ Assets/Scripts/UseStar.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using GCT.UI;
 
 public class UseStar : MonoBehaviour
 {
@@ -7,4 +8,21 @@ public class UseStar : MonoBehaviour
     public InputField m_UsageInput;
     public InputField m_UseStarInput;
     public Text m_Date;
+
+    private void Start()
+    {
+        m_UseStarInput.onValueChanged.AddListener(OnIntegerValueChange);
+    }
+
+    void OnIntegerValueChange(string inputContent)
+    {
+        if (inputContent.IndexOf('.') >= 0)
+        {
+            m_UseStarInput.text = inputContent.Replace(".", "");
+        }
+        if (inputContent.IndexOf(',') >= 0)
+        {
+            m_UseStarInput.text = inputContent.Replace(",", "");
+        }
+    }
 }
